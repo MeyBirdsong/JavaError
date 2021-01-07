ae58cfc3f8fc42803d0687656ab16ed81c56b277
MeyBirdsong <74513901+MeyBirdsong@users.noreply.github.com>
2021-01-07 15:12:02 -0600

    Add files via upload

---
+++ NewProject/src/com/company/Main.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Person me = new Person(String name)
+        System.out.println(me );
+
+      //  System.out.println("Hello Java");
+     //   System.out.println("Hi");
+       // System.out.print("Hey");
+
+        Scanner input = new Scanner(System.in);
+
+      //  System.out.println("What is your name?");
+       // String firstName = input.nextLine();
+        // slide 21
+       // System.out.printf("Hello %s!\n", firstName);
+
+        // Getting lastname from the user
+      // System.out.println("What is your last name?");
+       // String lastName = input.nextLine();
+       // System.out.printf("Your entire name is %s %s.\n", firstName,lastName);
+
+     //   System.out.println("Enter a name: ");
+     //   String name = input.nextLine();
+     //   System.out.println("Give me an adverb: ");
+      //  String adverb = input.nextLine();
+     //   System.out.println("Give me a adjective: ");
+     //   String adjective = input.nextLine();
+    //    System.out.println("Give me a verb: ");
+   //     String verb = input.nextLine();
+    //    System.out.println("Give me a noun two: ");
+   //     String nounTwo = input.nextLine();
+   //     System.out.println("Give me a number: ");
+   //     String number = input.nextLine();
+   //     System.out.println("Give me your name: ");
+     //   String yourName = input.nextLine();
+     //   System.out.printf("Here is your story: \n");
+     //   System.out.printf("\n You are %s %s and I want to be your %s!", adverb, adjective, verb);
+     //   System.out.printf("\n I want to go to the %s with you in %s days.", nounTwo, number);
+    //    System.out.printf("\n Sincerely, %s \n", yourName);
+
+
+    }
+}
---
+++ NewProject/src/com/company/PersonTwo.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class PersonTwo  extends Person {
+    public PersonTwo (String name, int age, int height)
+    super(name, age, height);
+
+}
